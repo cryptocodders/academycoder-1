69849075fbebbc9e0ab9ca9841bc48bce5c17f96
fabricioalfa <104047729+fabricioalfa@users.noreply.github.com>
2024-04-18 23:51:13 -0400

    Update storage.sol

--- Clase 03/parteFabri/storage.sol
+++ Clase 03/parteFabri/storage.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.22;
 
+contract AlmacenamientoYMemoria {
+    // Variable de almacenamiento: Permanece en la cadena de bloques
+    uint256 public datosPermanentes;
+
+    // Función para actualizar los datos permanentes
+    function actualizarDatosPermanentes(uint256 _nuevoDato) public {
+        datosPermanentes = _nuevoDato;
+    }
+
+
+    // Función que utiliza la memoria para almacenar datos temporalmente
+    function almacenarTemp(uint256 _tempData) public pure returns (uint256) {
+        // Variable local que utiliza la memoria
+        uint256 temp;
+        temp = _tempData * 2; // Realiza alguna operación temporal
+        return temp; // Devuelve el resultado temporal
+    }
+}
